add3e12f384c216cdbc2432cb9c060d8abf5ecaf
amar117 <kamarnath009@gmail.com>
2021-01-18 16:26:21 +0530

    mark as read

--- libs/books/feature/src/lib/reading-list/reading-list.component.ts
+++ libs/books/feature/src/lib/reading-list/reading-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { getReadingList, removeFromReadingList } from '@tmo/books/data-access';
+import { getReadingList, removeFromReadingList, markFinished } from '@tmo/books/data-access';
 
 @Component({
   selector: 'tmo-reading-list',
@@ -14,5 +14,10 @@ export class ReadingListComponent {
 
   removeFromReadingList(item) {
     this.store.dispatch(removeFromReadingList({ item }));
+  }
+
+  addBookToFinished(item) {
+    this.store.dispatch(markFinished({ item }));
+    this.readingList$ = this.store.select(getReadingList);
   }
 }
